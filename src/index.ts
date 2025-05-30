640a534036886f950f1bff44b7ddb162c416995f
Salvador Praxis <salvariable@icloud.com>
2025-05-30 09:23:55 -0600

    Allow array of notes

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,39 @@ const { AudioModule } = NativeModules;
 
 export type WaveType = 'sine' | 'square' | 'triangle' | 'sawtooth';
 
-type PlayToneParams = {
+export type PlayToneParams = {
   frequency: number;
   duration?: number; // in seconds
   waveform?: WaveType;
 };
 
-export function playTone({ frequency, duration = 0.5, waveform = 'sine' }: PlayToneParams): void {
+export function playTone(input: PlayToneParams | PlayToneParams[]): void {
+  const duration = (note: PlayToneParams) => note.duration ?? 1.5;
+  const waveform = (note: PlayToneParams) => note.waveform ?? 'sine';
+
   if (Platform.OS === 'ios' || Platform.OS === 'android') {
-    const durationMs = duration * 1000;
-    AudioModule?.playToneWithWave?.(frequency, durationMs, waveform);
+    if (Array.isArray(input)) {
+      for (const note of input) {
+        AudioModule?.playToneWithWave(note.frequency, duration(note) * 1000, waveform(note));
+      }
+    } else {
+      AudioModule?.playToneWithWave(input.frequency, duration(input) * 1000, waveform(input));
+    }
   } else if (Platform.OS === 'web') {
-    playToneWeb(frequency, duration, waveform);
+    if (Array.isArray(input)) {
+      for (const note of input) {
+        playToneWeb(note.frequency, duration(note) * 1000, waveform(note));
+      }
+    } else {
+      playToneWeb(input.frequency, duration(input) * 1000, waveform(input));
+    }
   } else {
-    console.warn('[pure-tone] Platform not supported.');
+    console.warn('[pure-tone] Unsupported platform or missing native module.');
   }
 }
 
-function playToneWeb(freq: number, duration: number, wave: WaveType): void {
-  const AudioContextClass = window.AudioContext || (window as any).webkitAudioContext;
+function playToneWeb(freq: number, durationMs: number, wave: WaveType): void {
+  const AudioContextClass = (window.AudioContext || (window as any).webkitAudioContext) as typeof AudioContext;
   const context = new AudioContextClass();
 
   const oscillator = context.createOscillator();
@@ -36,5 +50,5 @@ function playToneWeb(freq: number, duration: number, wave: WaveType): void {
   gainNode.connect(context.destination);
 
   oscillator.start();
-  oscillator.stop(context.currentTime + duration);
+  oscillator.stop(context.currentTime + durationMs / 1000);
 }
